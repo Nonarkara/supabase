ef222dc4aa1bf2cd13fa21ff24b636f05ee74b21
Chris Chinchilla <chris.ward@supabase.io>
2026-07-03 17:26:22 +0200

    Small page layout tweak

--- apps/docs/layouts/DefaultLayout.tsx
+++ apps/docs/layouts/DefaultLayout.tsx
@@ -4,4 +4,4 @@ import { cn } from 'ui'
 export const LayoutMainContent: FC<PropsWithChildren<{ className?: string }>> = ({
   className,
   children,
-}) => <div className={cn('max-w-6xl px-8 mx-auto pt-12 pb-8', className)}>{children}</div>
+}) => <div className={cn('max-w-6xl px-6 mx-auto pt-12 pb-6', className)}>{children}</div>
